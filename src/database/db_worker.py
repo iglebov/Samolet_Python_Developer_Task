0479c2f1ca7a5f644f24f4ffe7b4e721ac3ee634
Ivan Glebov <iglebov40@gmail.com>
2024-09-08 13:47:15 +0300

    feat: Connect To DataBase

--- src/database/db_worker.py
+++ src/database/db_worker.py
@@ -1,22 +1,39 @@
-from typing import Any, Dict, Union
+from typing import Any
+
+import pandas as pd
+import psycopg2
+import streamlit as st
 
 
 class DBworker:
-    def __init__(self, st):
-        self.connection = st.connection("postgresql", type="sql")
+    def __init__(
+        self,
+    ):
+        self.connection = psycopg2.connect(
+            user=st.secrets["username"],
+            host=st.secrets["host"],
+            port=st.secrets["port"],
+            password=st.secrets["password"],
+            database=st.secrets["database"],
+        )
 
-    def insert_row(self, fruit_info: Dict[str, Union[str, list]]) -> None:
+    def select(self) -> pd.DataFrame:
+        return pd.read_sql("SELECT * FROM fruits_info", self.connection).rename(
+            columns={
+                "day": "День недели",
+                "tree_name": "Название дерева",
+                "fruits_number": "Кол-во фруктов",
+            }
+        )
+
+    def insert(self, fruit_info: pd.Series) -> None:
         insert_query = f"""INSERT INTO fruits_info (day, tree_name, fruits_number)
-                                VALUES
-                                (
-                                {fruit_info.get("day")},
-                                {fruit_info.get("tree_name")},
-                                {fruit_info.get("fruits_number")}
-                                );"""
-        self.connection.query(insert_query, ttl="10m")
+        VALUES ('{fruit_info["День недели"]}', '{fruit_info["Название дерева"]}', {fruit_info["Кол-во фруктов"]});"""
+        cursor = self.connection.cursor()
+        cursor.execute(insert_query)
         self.connection.commit()
 
-    def update_row(
+    def update(
         self,
         update_column: str,
         new_value: Any,
@@ -25,15 +42,13 @@ class DBworker:
         column_2,
         value_2,
     ) -> None:
-        update_query = f"""UPDATE fruits_info
-                           SET {update_column} = {new_value}
-                           WHERE {column_1} = {value_1} AND {column_2} = {value_2};"""
-        self.connection.query(update_query, ttl="10m")
+        update_query = f"""UPDATE fruits_info SET {update_column} = {new_value}
+        WHERE {column_1} = {value_1} AND {column_2} = {value_2};"""
+        cursor = self.connection.cursor()
+        cursor.execute(update_query)
         self.connection.commit()
 
-    def delete_data(self):
-        self.connection.query("TRUNCATE fruits_info", ttl="10m")
+    def clear(self):
+        cursor = self.connection.cursor()
+        cursor.execute("TRUNCATE fruits_info")
         self.connection.commit()
-
-    def finish(self):
-        self.connection.close()
--- src/datatypes/fruit_frame.py
+++ src/datatypes/fruit_frame.py
@@ -13,12 +13,8 @@ from src.dynamic_filters import DynamicFilters
 class FruitFrame:
     def __init__(self, st: streamlit):
         self.st = st
-        self.db = DBworker(st)
-        # self.data = pd.DataFrame(
-        #     columns=["День недели", "Название дерева", "Кол-во фруктов"],
-        #     data=[],
-        # )
-        self.data = None
+        self.db = DBworker()
+        self.data = self.db.select()
 
     def form(self) -> None:
         with self.st.form(key="fruits_info"):
@@ -36,14 +32,15 @@ class FruitFrame:
                 if fruits_number is None or fruits_number < 0:
                     self.st.warning("Пожалуйста, укажите неотрицательное число плодов!")
                 elif tree_name:
-                    series = pd.Series(
+                    fruit_info = pd.Series(
                         {
                             "День недели": day_name,
                             "Название дерева": tree_name,
                             "Кол-во фруктов": fruits_number,
                         }
                     )
-                    self.insert(series)
+                    self.insert(fruit_info)
+
                     success = self.st.success("Информация успешно добавлена!")
                     time.sleep(DEFAULT_SLEEP_TIME)
                     success.empty()
@@ -52,27 +49,11 @@ class FruitFrame:
                     time.sleep(DEFAULT_SLEEP_TIME)
                     warning.empty()
 
-    def insert(self, series: pd.Series) -> None:
-        # TODO: Проверка валидации данных (тот же день + плод) | Работа с DataFrame
-        # TODO: Обновляем одну строку в БД (Update)
-        # Если уже существует, то обновляем количество плодов
-        # if (
-        #     (self.data["День недели"] == series["День недели"])
-        #     and (self.data["Название дерева"] == series["Название дерева"])
-        # ).any():
-        #     row_index = self.data.index[
-        #         self.data["День недели"] == "David"
-        #         and self.data["Название дерева"] == "..."
-        #     ].tolist()
-        #     self.data.iloc[row_index]["Кол-во плодов"] = series["Кол-во плодов"]
-        # Если не существует, то просто добавляем
-        # else:
+    def insert(self, fruit_info: pd.Series) -> None:
+        self.db.insert(fruit_info)
         updated_data = pd.concat(
-            [self.data, pd.DataFrame([series], columns=series.index)]
+            [self.data, pd.DataFrame([fruit_info], columns=fruit_info.index)]
         )
-        # TODO: Добавляем одну строку в БД (INSERT)
-        self.db.insert_row(...)
-        #
         self.data = updated_data.sort_values(
             by="День недели", key=lambda day: DAYS_SERIES[day]
         )
@@ -81,21 +62,18 @@ class FruitFrame:
         changed_data = self.st.data_editor(
             self.data, hide_index=True, use_container_width=True
         )
-        # Валидация
-        # Если не ок, то выводим ошибку
-
-        # Если ок, то сравниваем DataFrames и UPDATE БД
-        # self.data.compare(changed_data)
-        # Определяем столбец для обновления, Определяем два других столбца и значения
-        # TODO: Добавляем одну строку в БД (INSERT)
-        self.db.update_row(...)
-        # Вносим обновлённые данные
-        ###
-        self.data = changed_data.sort_values(
-            by="День недели", key=lambda day: DAYS_SERIES[day]
-        )
+        if self._data_validated(changed_data):
+            self.db.update(...)
+            self.data = changed_data.sort_values(
+                by="День недели", key=lambda day: DAYS_SERIES[day]
+            )
+        else:
+            self.st.warning("...")
 
     def output(self) -> None:
+        self.data = self.db.select().sort_values(
+            by="День недели", key=lambda day: DAYS_SERIES[day]
+        )
         df = DynamicFilters(
             self.data, filters=["День недели", "Название дерева", "Кол-во фруктов"]
         )
@@ -122,4 +100,28 @@ class FruitFrame:
 
     def clear(self) -> None:
         self.data = self.data[0:0]
-        self.db.delete_data()
+        self.db.clear()
+
+    def _data_validated(self, changed_data: pd.DataFrame) -> bool:
+        compared_df = self.data.compare(changed_data)
+        row = compared_df.index.values[0].item()
+        column = compared_df.columns[0][0]
+        value = changed_data[column][row]
+
+        if column == "День недели" and value not in DAYS_LIST:
+            self.st.warning("Пожалуйста, укажите корректный день недели.")
+            return False
+        return True
+
+        # Если уже существует, то обновляем количество плодов
+        # if (
+        #     (self.data["День недели"] == series["День недели"])
+        #     and (self.data["Название дерева"] == series["Название дерева"])
+        # ).any():
+        #     row_index = self.data.index[
+        #         self.data["День недели"] == "David"
+        #         and self.data["Название дерева"] == "..."
+        #     ].tolist()
+        #     self.data.iloc[row_index]["Кол-во плодов"] = series["Кол-во плодов"]
+        # Если не существует, то просто добавляем
+        # else:
